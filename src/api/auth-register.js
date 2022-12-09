a9556c8ec31e36c1dbed4aa454bf535fbc51709b
Kristina Salnyk <kristina.salnyk@gmail.com>
2022-12-09 15:31:51 +0100

    Update auth-register.js

--- src/api/auth-register.js
+++ src/api/auth-register.js
@@ -1,6 +1,7 @@
-import {api} from './api';
+import { api, clearAuthHeader } from './api';
 
 export const authRegister = async user => {
+  clearAuthHeader();
   const response = await api.post('/api/v1/auth/reg', user);
   return response.data;
 };
